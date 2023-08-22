2624612bbbc158fd93916ea1b17e85bef277c9d9
Pratyasa-Ray <pratyasaray1912@gmail.com>
2023-08-22 14:55:02 +0530

    updating file

--- l1q10.c
+++ l1q10.c
@@ -2,19 +2,23 @@
 #include<sys/stat.h>
 #include<sys/types.h>
 #include<stdlib.h>
-#include<fcntl.h>
 #include<errno.h>
 #include<fcntl.h>
 #include<unistd.h>
 int main(){
-	int fd=open("t1",O_RDWR|O_CREAT|O_TRUNC,S_IRUSR|S_IWUSR);
+	int fd=open("t1",O_RDWR|O_CREAT,S_IRUSR|S_IWUSR);
 	if(fd== -1)
-		errExit("open");
-	ssize_t write(fd,"l1q8",10);
-	off_t lseek(fd,10,SEEK_END);
-	ssize_t write(fd,"l1q8",10);
+		printf("open");
+	char content[10];
+	printf("enter the contents: ");
+	scanf(" %[^\n]",content);
+	write(fd,content,10);
+	lseek(fd,10L,SEEK_END);
+	write(fd,content,10);
 	int curr=lseek(fd,0,SEEK_CUR);
 	printf("the current offset value: %d \n",curr);
+	return 0;
+}
 
 
 
